42cd707f16fb0dcf762e02c4aa6b9906c5396de3
TheTrollGodJr <thetrollgodjr@gmail.com>
2025-11-14 18:19:50 -0700

    Created FaceAlignment csproj. Renamed library and namespace. Started work on exe app

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,13 +1,73 @@
-using FaceONNX;
+using FaceAlignment;
 using System;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
-using FaceManipulator;
+using FaceONNX;
 
 class Program {
 
-	static void Main() {
+    private List<string> buffer = new List<string>();
+
+    static void Main() {
+        
+        Print("this /ris /ga /btest/w!"); 
+        //Dialogue();
+    }
+
+    private static void Diaglogue() {
+        
+        Console.WriteLine();
+    }
+
+    private static int Selection(int inputType, string display, List<string> options) {
+        return 0;
+    }
+
+    private static void Print(string display) {
+        
+        bool changeColor = false;
+
+        foreach (char c in display) {
+            
+            if (c == '/') {
+
+                changeColor = true;
+                continue;
+            }
+            else if (changeColor) {
+                
+                changeColor = false;
+                switch (c) {
+                    
+                    case 'r':
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        continue;
+                    case 'g':
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        continue;
+                    case 'b':
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        continue;
+                    case 'y':
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        continue;
+                    case 'w':
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        continue;
+                }
+            }
+
+            Console.Write(c);
+        }
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+	static void old() {
 		var fd = new FaceDetector();
 		var landmarks = new Face68LandmarksExtractor();
 		var face = new Face("images/img-2.jpg");
